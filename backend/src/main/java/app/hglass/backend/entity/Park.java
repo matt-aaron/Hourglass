b28500e0ec9d4e427b6b67ac39c7e433766449ee
Matt Aaron <13080357+msaaron@users.noreply.github.com>
2019-02-26 09:36:32 -0500

    Add getters and location

--- backend/src/main/java/app/hglass/backend/entity/Park.java
+++ backend/src/main/java/app/hglass/backend/entity/Park.java
@@ -1,15 +1,17 @@
 package app.hglass.backend.entity;
 
+import app.hglass.backend.model.Location;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import javax.persistence.*;
+import java.util.List;
 
 /**
  * Implements a class representing an amusement park. This class maintains the relationships necessary to tie
  * information from the database together, such as rides and wait times.
  *
  * @author Matt Aaron
- * @version 0.0.1
+ * @version 0.0.2
  */
 @Entity
 @Table(name = "parks")
@@ -27,9 +29,66 @@ public class Park {
 	private String name;
 
 	/**
-	 * Park many-to-one relationship with operators
+	 * Embedded <code>Location</code> object
+	 */
+	@Embedded
+	private Location location;
+
+	/**
+	 * Many-to-one relationship with <code>Operator</code>
 	 */
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JsonIgnore
 	private Operator operator;
+
+	/**
+	 * One-to-many relationship with <code>RideType</code>s
+	 */
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "park")
+	private List<RideType> rideTypes;
+
+	/**
+	 * Get unique identifier
+	 *
+	 * @return unique identifier
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * Get park name
+	 *
+	 * @return park name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * Get park <code>Location</code>
+	 *
+	 * @return park <code>Location</code>
+	 */
+	public Location getLocation() {
+		return location;
+	}
+
+	/**
+	 * Get park <code>Operator</code>
+	 *
+	 * @return park <code>Operator</code>
+	 */
+	public Operator getOperator() {
+		return operator;
+	}
+
+	/**
+	 * Get list of the ride types/categories
+	 *
+	 * @return list of <code>RideType</code>s
+	 */
+	public List<RideType> getRideTypes() {
+		return rideTypes;
+	}
 }
