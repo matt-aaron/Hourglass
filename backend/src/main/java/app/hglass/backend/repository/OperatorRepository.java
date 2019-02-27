b3f63ffde483569e97ef7c972d7c4f3d2917304e
Matt Aaron <13080357+msaaron@users.noreply.github.com>
2019-02-26 19:01:18 -0500

    Update OperatorRepository

--- backend/src/main/java/app/hglass/backend/repository/OperatorRepository.java
+++ backend/src/main/java/app/hglass/backend/repository/OperatorRepository.java
@@ -2,6 +2,15 @@ package app.hglass.backend.repository;
 
 import app.hglass.backend.entity.Operator;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+/**
+ * Implements a repository for operator information from the database.
+ *
+ * @author Matt Aaron
+ * @version 0.0.1
+ */
+@Repository
 public interface OperatorRepository extends JpaRepository<Operator, Integer> {
+
 }
