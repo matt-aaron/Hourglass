f5486ee1adbb9eff66f9c35b5a5c3da24d77714c
Matt Aaron <13080357+msaaron@users.noreply.github.com>
2019-03-01 23:46:23 -0500

    Add getters and setters

--- backend/src/main/java/app/hglass/backend/entity/Ride.java
+++ backend/src/main/java/app/hglass/backend/entity/Ride.java
@@ -4,6 +4,12 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import javax.persistence.*;
 
+/**
+ * Implements an entity class representing rides from the database.
+ *
+ * @author Matt Aaron
+ * @version 0.0.2
+ */
 @Entity
 @Table(name = "rides")
 public class Ride {
@@ -44,4 +50,94 @@ public class Ride {
 	 */
 	@Transient
 	private RideStatus status;
+
+	/**
+	 * Get unique identifier
+	 *
+	 * @return ride unique identifier
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * Get name
+	 *
+	 * @return name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * Set name
+	 *
+	 * @param name new name
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * Get TE2 platform identifier
+	 *
+	 * @return TE2 platform identifier
+	 */
+	public String getPlatformId() {
+		return platformId;
+	}
+
+	/**
+	 * Set TE2 platform identifier
+	 *
+	 * @param platformId new TE2 platform identifier
+	 */
+	public void setPlatformId(String platformId) {
+		this.platformId = platformId;
+	}
+
+	/**
+	 * Get <code>Park</code>
+	 *
+	 * @return <code>Park</code>
+	 */
+	public Park getPark() {
+		return park;
+	}
+
+	/**
+	 * Set <code>Park</code>
+	 *
+	 * @param park new <code>Park</code>
+	 */
+	public void setPark(Park park) {
+		this.park = park;
+	}
+
+	/**
+	 * Get <code>RideType</code>
+	 *
+	 * @return <code>RideType</code>
+	 */
+	public RideType getType() {
+		return type;
+	}
+
+	/**
+	 * Set <code>RideType</code>
+	 *
+	 * @param type new <code>RideType</code>
+	 */
+	public void setType(RideType type) {
+		this.type = type;
+	}
+
+	/**
+	 * Get <code>RideStatus</code>. This field is initialized by the REST controller.
+	 *
+	 * @return <code>RideStatus</code>
+	 */
+	public RideStatus getStatus() {
+		return status;
+	}
 }
