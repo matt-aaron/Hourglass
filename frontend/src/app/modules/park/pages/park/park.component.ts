3e8b881103299ff463d395872d1737245022f601
Matt Aaron <13080357+msaaron@users.noreply.github.com>
2019-03-07 15:47:20 -0500

    Improve Park component data handling and routing

--- frontend/src/app/modules/park/pages/park/park.component.ts
+++ frontend/src/app/modules/park/pages/park/park.component.ts
@@ -14,38 +14,83 @@ export class ParkComponent implements OnInit, OnDestroy {
 
   private routeSubscription: Subscription;
   private park: Park;
+  private error: boolean;
+  private loading: boolean;
 
   constructor(
     private operatorService: OperatorService,
     private rideTypeService: RideTypeService,
     private route: ActivatedRoute,
     private router: Router
-  ) {
-  }
+  ) { }
 
   ngOnInit(): void {
-    this.routeSubscription = this.route.params.subscribe(params => {
+    // Make sure the error card is hidden initially
+    this.error = false;
+
+    this.routeSubscription = this.route.firstChild.params.subscribe(params => {
+      // Set the park pointer to null
+      this.park = null;
+
+      // Display the loading indicator
+      this.loading = true;
+
+      // Fetch the operators from the API
       this.operatorService.getAll()
         .subscribe(response => {
-          response.forEach((operator: Operator) => {
-            operator.parks.forEach((park: Park) => {
-              if (park.id === +params.id) {
-                this.park = park;
-              }
+          // Check if the ID has been specified in the route
+          if (params.id) {
+            // Loop through the operators and parks to locate the target park
+            response.forEach((operator: Operator) => {
+              operator.parks.forEach((park: Park) => {
+                if (park.id === +params.id) {
+                  // Matching park ID fount
+                  // Set the park pointer to this park
+                  this.park = park;
+
+                  // Make sure the error card is hidden
+                  this.error = false;
+                }
+              });
             });
-          });
-
-
-          if (typeof this.park === undefined) {
+          } else {
+            // No park ID was specified, so we'll go ahead and route them to the first park in the API response
+            console.log('park not found');
             this.router.navigate(['/park', response[0].parks[0].id]);
+            this.park = response[0].parks[0];
           }
 
+          // If the park ID wasn't found, let the user know there was an error
+          if (this.park == null) {
+            // Display the error card
+            this.error = true;
+
+            // Hide the loading indicator
+            this.loading = false;
+
+            // Exit the method
+            return;
+          }
+
+          // Fetch the ride types for this park
           this.rideTypes$ = this.rideTypeService.getByPark(this.park.id);
+          this.rideTypes$.subscribe(rtResponse => {
+            // Hide the loading indicator
+            this.loading = false;
+          });
         });
     });
   }
 
   ngOnDestroy(): void {
     this.routeSubscription.unsubscribe();
+  }
+
+  hasError(): boolean {
+    return this.error;
+  }
+
+  isLoading(): boolean {
+    return this.loading;
   }
 }
