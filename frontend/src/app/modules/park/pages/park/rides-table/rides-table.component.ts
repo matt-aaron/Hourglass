ba324cf0cb2bf9d1db1320524c188c00eb4ab22d
Matt Aaron <13080357+msaaron@users.noreply.github.com>
2019-03-07 21:43:39 -0500

    Fully implement rides table

--- frontend/src/app/modules/park/pages/park/rides-table/rides-table.component.ts
+++ frontend/src/app/modules/park/pages/park/rides-table/rides-table.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
-
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { RideType } from '@app/core';
 
 @Component({
@@ -7,9 +6,41 @@ import { RideType } from '@app/core';
   templateUrl: './rides-table.component.html',
   styleUrls: ['./rides-table.component.css']
 })
-export class RidesTableComponent implements OnInit {
+export class RidesTableComponent implements OnChanges {
   @Input() rideTypes: RideType[];
-  constructor() { }
 
-  ngOnInit() { }
+  private activeRideType: RideType;
+
+  constructor() {
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.rideTypes && this.rideTypes !== null) {
+      // Sort the rides by name
+      this.rideTypes.forEach((rideType: RideType) => {
+        rideType.rides.sort((a, b) => {
+          if (a.name < b.name) {
+            return -1;
+          } else if (a.name > b.name) {
+            return 1;
+          } else {
+            return 0;
+          }
+        });
+      });
+      this.activeRideType = this.rideTypes[0];
+    }
+  }
+
+  isActiveRideType(type: RideType): boolean {
+    return type === this.activeRideType;
+  }
+
+  getActiveRideType(): RideType {
+    return this.activeRideType;
+  }
+
+  setActiveRideType(type: RideType): void {
+    this.activeRideType = type;
+  }
 }
