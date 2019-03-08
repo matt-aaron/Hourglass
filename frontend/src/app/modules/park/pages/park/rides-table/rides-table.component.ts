6f4a0a4feb7fd46c3758ebb9e624904b298ece43
Matt Aaron <13080357+msaaron@users.noreply.github.com>
2019-03-08 15:22:53 -0500

    Clean up display of wait times in the rides table

--- frontend/src/app/modules/park/pages/park/rides-table/rides-table.component.ts
+++ frontend/src/app/modules/park/pages/park/rides-table/rides-table.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
-import { RideType } from '@app/core';
+import { Ride, RideType } from '@app/core';
 
 @Component({
   selector: 'app-rides-table',
@@ -43,4 +43,21 @@ export class RidesTableComponent implements OnChanges {
   setActiveRideType(type: RideType): void {
     this.activeRideType = type;
   }
+
+  getFormattedWaitTime(ride: Ride): string {
+    if (!ride.status.open) {
+      // The ride is closed
+      return '<span class="text-muted">&#9679;</span> Closed';
+    }
+
+    // The ride is open, so we need to display the correct colored indicator next to the wait time in the table
+    if (ride.status.waitTime <= 20) {
+      return `<span class="text-success">&#9679;</span> ${ride.status.waitTime} minutes`;
+    } else if (ride.status.waitTime <= 40) {
+      return `<span class="text-warning">&#9679;</span> ${ride.status.waitTime} minutes`;
+    } else {
+      return `<span class="text-danger">&#9679;</span> ${ride.status.waitTime} minutes`;
+    }
+  }
+
 }
